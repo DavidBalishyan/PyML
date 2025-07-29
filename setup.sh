691df4314c51f977e9a8c1cdd32647e95285f9c4
DavidBalishyan <davidbalishyan12@gmail.com>
2025-07-29 14:31:11 +0400

    Added: setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# conda install --yes --file requirements.txt
+
+curl https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv > data.csv
