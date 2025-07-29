162ec9c4377245fdcb03ae6bd1b231a95fba313f
DavidBalishyan <davidbalishyan12@gmail.com>
2025-07-29 16:28:03 +0400

    Updated: setup.sh, wrote some code

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# Final ML project
-[Mihran Miqaelyan]("https://github.com/Mihrhah")
-[Gurgen Aghajanyan]("https://github.com/Gurgen2012")
+# Final project for TUMO ML Lab 
+
 
 
 [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](
 https://colab.research.google.com/github/DavidBalishyan/PyML/blob/main/main.ipynb)
+
+# Contributors
+
+1. <a href="https://github.com/MIhrhah">Mihran Miqaelyan</a>
+</br>
+2. <a href="https://github.com/Gurgen2012">Gurgen Aghajanyan (debil)</a>
--- setup.sh
+++ setup.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-# conda install --yes --file requirements.txt
+# -----------------------------------------------------------------
+# setup.sh - Script to set up the environment for PyML project
+# -----------------------------------------------------------------
+# Environment setup
+# ----------------------------------------------------------------
+# Check if conda is installed
+if command -v conda &> /dev/null; then
+    echo "Conda is installed. Proceeding with conda environment setup."
+    # Create a conda environment
+    conda create --name tf tensorflow --yes # If you use gpu tensorflow use `tensorflow-gpu` instead of `tensorflow`
+    # Activate the conda environment
+    conda activate tf
+else
+    echo "Conda is not installed. Proceeding with pip environment setup. "
+    # Check if pip is installed
+    if command -v pip3 &> /dev/null; then
+        echo "Pip is installed. Proceeding with pip environment setup."
+        # Create a virtual environment using pip
+        python3 -m venv venv
+        # Activate the virtual environment
+        source venv/bin/activate
+    else
+        echo "Neither conda nor pip is installed. Please install one of them to proceed."
+        exit 1
+    fi
+fi
 
-curl https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv > data.csv
+# Install dependencies
+# ----------------------------------------------------------------
+# Uncomment the following line to install dependencies using conda
+# conda install --yes --file requirements.txt
+# ----------------------------------------------------------------
+# If you are using Pypi, uncomment the following line
+# pip3 install -r requirements.txt
